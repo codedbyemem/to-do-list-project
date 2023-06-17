3d5b37da300cb360efbca29be3db96145d32f6f5
codedbyemem <eumoabasi@gmail.com>
2023-06-17 23:48:56 +0100

    git commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+let input = (prompt("Please enter a command"))
+let list = []
+
+if (input === "QUIT") {
+console.log("Ok, you quit")}
+else if(input === "NEW"){
+    let listItem = (prompt("Add an item to your to-do list"))
+    list.push(listItem);{
+console.log(`You have added ${listItem} to the list`)
+    }}
+else if(input === "DELETE"){
+
+
+
+
+    
+}
+
+
